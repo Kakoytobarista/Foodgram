a41021ed698fdafbc88fc339d955072cb28e3f5b
Aslan <HeyAslan@yandex.ru>
2022-07-16 14:01:48 +0200

    Delete import

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -6,7 +6,6 @@ from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework.response import Response
 
 from api.filters import RecipeFilterSet
 from api.mixins import ListRetrieveViewSet
